6abba9399f119a68efe85043fad8d28ed79d2f5e
K N Deepthi <47886596+deepthi0697@users.noreply.github.com>
2020-05-01 15:01:21 +0530

    Update setupDB.js

--- config/setupDB.js
+++ config/setupDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const setupDB = (req, res) => {
-    mongoose.connect('mongodb+srv://deepthi:shaz7022@cluster0-7uxoi.mongodb.net/Twitter?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect('mongodb+srv://deepthi:*********@cluster0-7uxoi.mongodb.net/Twitter?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('connected to DB')
     })
